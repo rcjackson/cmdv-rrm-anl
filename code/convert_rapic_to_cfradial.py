61414f2f246492fde7534d393129d986d936de3a
Robert Jackson <rjackson@anl.gov>
2017-02-01 10:42:06 -0600

    ADD: Flag to choose whether to convert rapic files in serial or parallel

--- code/convert_rapic_to_cfradial.py
+++ code/convert_rapic_to_cfradial.py
@@ -1,25 +1,14 @@
-## Converts rapic files in given directory to Cf/Radial format
+import os
 
-import pyart
-import glob
+out_dir = '/home/rjackson/data/radar/berr/2009'
+in_dir = '/lcrc/group/earthscience/radar/stage/radar_disk_two/berr_rapic/berr_2009/'
 
-data_file_path = '/home/rjackson/data/radar/berr/berr_2009'
-
-# Get list of radar files (recursively)
-
-file_list = glob.glob(data_file_path + '/**/*.rapic', recursive=True)
-print('Converting ' + str(len(file_list)) + ' files to Cf/Radial')
-
-for radar_file in file_list:
-    Radar = pyart.io.read_rsl(radar_file)
-    out_file_name = radar_file[0:-5] + '.cfradial'
-    pyart.io.write_cfradial(out_file_name, Radar)
-
-
+for root, dirs, files in os.walk(in_dir, topdown=False):
+    for name in dirs:
+        os.chdir(os.path.join(root, name))
+        print('In directory ' + os.path.join(root, name))
+        os.system('RadxConvert -f *.rapic -v -outdir ' + out_dir)
   
-
-
-
 
 
 
